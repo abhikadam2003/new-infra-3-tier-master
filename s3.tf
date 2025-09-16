73730e9e1766ff392a1f669cf28c59c629244775
abhikadam2003 <kadamabhi7875@gmail.com>
2025-09-17 00:48:36 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 # Create an S3 Bucket
-resource "aws_s3_bucket" "example_bucket" {
+resource "aws_s3_bucket" "bucket" {
   bucket = "swiggy-bucket-terraform-dfgshjxkeiuedujehitrdemoprokect"
 
   # Enable versioning
